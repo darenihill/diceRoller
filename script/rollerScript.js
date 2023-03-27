1cf2342e75940fd4e6394e74c29712712556733a
darenihill <darenihill@gmail.com>
2023-03-27 13:13:54 -0400

    Revert "Slowed down animation."
    This reverts commit f51a3fad9cf792bbd0484f77324073ad7ee0faed.

--- script/rollerScript.js
+++ script/rollerScript.js
@@ -255,65 +255,53 @@ function getRandomNumber(min, max) {
 }
 
 function rollDice() {
-        const animationDuration = 1500; // Define the animation duration in milliseconds
         const diceToRoll = diceList.filter(dice => !dice.classList.contains("dice-held"));
-    
+
         if (diceToRoll.length === 0) {
-            alert("All dice are held!");
-            return;
-        }
-    
-        let rollTotal = 0; // Add a variable to keep track of the roll total
-    
-        diceToRoll.forEach((dice, index) => {
-            const number = dice.querySelector(".number");
-            const facesInput = dice.querySelector("input[type='hidden'].faces");
-            if (!facesInput) {
-                console.error("Dice is missing faces input:", dice);
+                alert("All dice are held!");
                 return;
-            }
-    
-            if (!dice.classList.contains("dice-held")) {
-                // Add shake animation class
-                dice.classList.add("shake");
-                // Remove shake animation class after animation completes
-                setTimeout(() => {
-                    dice.classList.remove("shake");
-                }, animationDuration);
-    
-                // Update the dice value after the shake animation has completed
-                setTimeout(() => {
-                    let newValue;
-                    if (dice.customFaces.length > 1) {
-                        newValue = dice.customFaces[getRandomNumber(0, dice.customFaces.length - 1)];
-                    } else {
-                        newValue = getRandomNumber(1, facesInput.value);
-                    }
-                    number.textContent = newValue;
-                    console.log(`Dice ${index + 1} roll: ${newValue}`); // Log each dice roll
-                }, animationDuration);
-            }
+        }
+
+        let rollTotal = 0; // Add a variable to keep track of the roll total
+
+        diceToRoll.forEach(dice => {
+                const number = dice.querySelector(".number");
+                const facesInput = dice.querySelector("input[type='hidden'].faces");
+                if (!facesInput) {
+                        console.error("Dice is missing faces input:", dice);
+                        return;
+                }
+                if (dice.customFaces.length > 1) {
+                        number.textContent = dice.customFaces[getRandomNumber(0, dice.customFaces.length - 1)];
+                } else {
+                        const newValue = getRandomNumber(1, facesInput.value);
+                        number.textContent = newValue;
+                }
+
+                if (!dice.classList.contains("dice-held")) {
+                        // Add shake animation class
+                        dice.classList.add("shake");
+                        // Remove shake animation class after animation completes
+                        setTimeout(() => {
+                                dice.classList.remove("shake");
+                        }, 500);
+                }
+
+
         });
-    
-        // Calculate roll total and display it after the shake animation has completed
-        setTimeout(() => {
-            // Calculate roll total for all dice, including held ones
-            diceList.forEach(dice => {
+
+        // Calculate roll total for all dice, including held ones
+        diceList.forEach(dice => {
                 const number = dice.querySelector(".number");
                 rollTotal += parseInt(number.textContent);
-            });
-    
-            // Display the roll total
-            const rollTotalContainer = document.querySelector(".roll-total-container");
-            const rollTotalElement = rollTotalContainer.querySelector("h1");
-            rollTotalElement.textContent = `Total: ${rollTotal}`;
-            rollTotalContainer.style.display = "flex";
-    
-            console.log(`Total roll: ${rollTotal}`); // Log the total roll
-        }, animationDuration);
-    }
-    
-    
+        });
+
+        // Display the roll total
+        const rollTotalContainer = document.querySelector(".roll-total-container");
+        const rollTotalElement = rollTotalContainer.querySelector("h1");
+        rollTotalElement.textContent = `Total: ${rollTotal}`;
+        rollTotalContainer.style.display = "flex";
+}
 
 
 function updateFontSize(dice) {
@@ -352,7 +340,7 @@ function toggleHoldAllDice() {
 
         allDice.forEach((die, index) => {
                 const holdButton = die.querySelector('.dice-button.hold');
-
+      
                 if (anyHeld && die.classList.contains('dice-held')) {
                         holdButton.click();
                 } else if (!anyHeld) {
